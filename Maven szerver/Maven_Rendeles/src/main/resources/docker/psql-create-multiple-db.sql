558d91f351c0cfdbbd29b07a4ce36bd631715fc2
Gerguraki555 <gergurakki@gmail.com>
2024-04-11 22:07:27 +0200

    TableSystem configurerd with docker

---
+++ Maven szerver/Maven_Rendeles/src/main/resources/docker/psql-create-multiple-db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE yokudlela_iam;
+CREATE DATABASE yokudlela_table;
+CREATE DATABASE yokudlela_menu;
+CREATE DATABASE yokudlela_waiter;
+
+GRANT ALL PRIVILEGES ON DATABASE yokudlela_iam TO yokudlela;
+GRANT ALL PRIVILEGES ON DATABASE yokudlela_table TO yokudlela;
+GRANT ALL PRIVILEGES ON DATABASE yokudlela_menu TO yokudlela;
+GRANT ALL PRIVILEGES ON DATABASE yokudlela_waiter TO yokudlela;
